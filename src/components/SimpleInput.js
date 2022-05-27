445789352e06b8a3f2cc137098da95f4d5042308
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-05-27 11:25:57 -0400

    Add state to form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,36 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+import { useRef, useState } from "react";
+
+const SimpleInput = (_) => {
+    const nameRef = useRef();
+    const [name, setName] = useState("");
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        // validate inputs
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-control">
+                <label htmlFor="name">Your Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    onChange={nameChangeHandler}
+                    value={name}
+                    ref={nameRef}
+                />
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
